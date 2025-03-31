d040839aec6c1bc9fa28d6eff3e9e376b69da223
Esteban Barone <estebanbaronepalacios@gmail.com>
2025-03-31 10:13:58 -0300

    Primera Prueba de Estban

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerMovement", menuName = "Scriptable Objects/PlayerMovement")]
+public class PlayerMovement : ScriptableObject
+{
+    // si leen éste mensaje manden empanadas.
+}
