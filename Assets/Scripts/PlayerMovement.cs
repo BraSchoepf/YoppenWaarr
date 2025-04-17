876acda20917c1077a1621a82e08b4f86583edf5
Fabián E. García <garciafabianenrique79@gmail.com>
2025-04-17 11:06:58 -0300

    creación y organización de carpetas

--- Assets/Scripts/PlayerMovement.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerMovement", menuName = "Scriptable Objects/PlayerMovement")]
-public class PlayerMovement : ScriptableObject
-{
-    // si leen éste mensaje manden empanadas.
-}
