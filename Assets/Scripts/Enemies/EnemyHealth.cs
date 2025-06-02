48626f9b51f518977dea279d2c64911212811c2a
Fabián E. García <garciafabianenrique79@gmail.com>
2025-06-02 19:00:56 -0300

    flashDamage funcional en enemigo

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int _maxHealth = 6;
     private int _currentHealth;
 
+    //effect for damage recive - for flashDamage()
+    public SpriteRenderer spriteRenderer;
+    private Color _originalColor;
+
     private void Start()
     {
         _currentHealth = _maxHealth;
+        _originalColor  = spriteRenderer.color;
     }
 
     public void TakeDamage(int amount)
     {
         _currentHealth -= amount;
         Debug.Log("El enemigo recibe daño, vida restante:: " + _currentHealth);
+
+        // Coroutina for flashDamage()
+        StartCoroutine(FlashDamage());
 
         if (_currentHealth <= 0)
         {
@@ -26,5 +35,21 @@ public class EnemyHealth : MonoBehaviour
         Debug.Log("ENEMY DOWN, COUNTER-TERRORIST WIN.");
         // I leave this space to add animations at a later date
         Destroy(gameObject);
+    }
+
+    //Coroutina for effect damage recive
+    IEnumerator FlashDamage()
+    {
+        int flashCount = 2;
+        float flashDuration = 0.1f;
+
+
+        for (int i = 0; i < flashCount; i++)
+        {
+            spriteRenderer.color = Color.red;
+            yield return new WaitForSeconds(flashDuration);
+            spriteRenderer.color = _originalColor;
+            yield return new WaitForSeconds(flashDuration);
+        }
     }
 }
