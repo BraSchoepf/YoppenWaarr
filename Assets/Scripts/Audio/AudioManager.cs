e1029e533d07a62984e73b826e32821f38417e4f
Fabián E. García <garciafabianenrique79@gmail.com>
2025-06-08 00:04:03 -0300

    cambio de AudioManager a escena menú, agregado de nuevas variables, codificación para button hover funcional

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,10 +2,33 @@ using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
 
+
 public class AudioManager : MonoBehaviour
 {
     // singletons
     public static AudioManager Instance { get; private set; }
+
+    // Música actual
+    private EventInstance musicInstance;
+
+    // Variables públicas para asignar en inspector
+    [Header("Música")]
+    public EventReference musica_menu;
+    public EventReference musica_pre_escenaTutorial;
+    public EventReference musica_escenaTutorial;
+    public EventReference musica_escenaCreditos;
+    public EventReference musica_escenaOpciones;
+    //public EventReference musica_boss;
+    //public EventReference musica_gameOver;
+    //public EventReference musica_victory;
+
+    [Header("SFX")]
+    public EventReference sfx_botonHover;
+    public EventReference sfx_play;
+    public EventReference sfx_dog;
+    public EventReference sfx_farm;
+    public EventReference sfx_lowLife;
+    public EventReference sfx_powerUp;
 
     private void Awake()
     {
@@ -18,6 +41,49 @@ public class AudioManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
+
+    // reproducir música
+    public void PlayMusic(EventReference musicEvent)
+    {
+        StopMusic();
+
+        if (musicEvent.IsNull)
+        {
+            Debug.LogWarning("Referencia de música nula");
+            return;
+        }
+
+        musicInstance = RuntimeManager.CreateInstance(musicEvent);
+        musicInstance.start();
+    }
+
+    // detener la música
+    public void StopMusic()
+    {
+        if (musicInstance.isValid())
+        {
+            musicInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+            musicInstance.release();
+            musicInstance.clearHandle(); // opcional pero recomendable
+        }
+    }
+
+
+    public void PlaySFX(EventReference sfxEvent)
+    {
+        if (sfxEvent.IsNull)
+        {
+            Debug.LogWarning("SFX vacío o no asignado.");
+            return;
+        }
+
+        RuntimeManager.PlayOneShot(sfxEvent);
+    }
+
+
+
+    //
 
     public void PlayOneShot(EventReference soundEvent, Vector3 position)
     {
@@ -37,4 +103,6 @@ public class AudioManager : MonoBehaviour
         instance.start();
         instance.release();
     }
+
+
 }
